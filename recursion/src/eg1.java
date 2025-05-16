717a6ea72d04bf00350fbc1cc26557e25cf61ce5
CJ2k4 <charitra.jain223@gmail.com>
2025-05-16 14:49:17 +0530

    Added recursion

---
+++ recursion/src/eg1.java
@@ -0,0 +1,13 @@
+public class eg1 {
+    public static void main(String[] args) {
+
+    }
+    public static int jos(int n, int k){
+
+        if(n==1) return 0;
+
+
+
+        return jos(n-1, k);
+    }
+}
