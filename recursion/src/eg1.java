48d9bb9f5f73f7a1065c93943a07ed97fc0457bd
CJ2k4 <charitra.jain223@gmail.com>
2025-05-16 15:02:12 +0530

    Josephus problem addes using recursion

--- recursion/src/eg1.java
+++ recursion/src/eg1.java
@@ -1,13 +1,12 @@
 public class eg1 {
     public static void main(String[] args) {
-
+        int n = jos(5,3);
+        System.out.println(n);
     }
     public static int jos(int n, int k){
 
         if(n==1) return 0;
 
-
-
-        return jos(n-1, k);
+        else return (jos(n-1, k) + k)%n;
     }
 }
