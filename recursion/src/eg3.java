fd0072e5661f6185a7d924a40e6c0f39731c785d
CJ2k4 <charitra.jain223@gmail.com>
2025-05-21 17:37:15 +0530

    Added Efficient solution of Water Trapping Problem

--- recursion/src/eg3.java
+++ recursion/src/eg3.java
@@ -1,2 +1,34 @@
 public class eg3 {
+
+    //Trapping Water Efficient Solution--------
+
+    public static void main(String[] args) {
+        int[] arr = {3, 0, 1, 2, 5};
+
+        System.out.println(getWater(arr));
+    }
+
+    static int getWater(int[] arr){
+        int res = 0;
+
+        int[] lmax = new int[arr.length];
+        lmax[0] = arr[0];
+
+        int[] rmax = new int[arr.length];
+        rmax[arr.length-1] = arr[arr.length -1];
+
+        for(int i = 1; i<arr.length; i++){
+            lmax[i] = Math.max(arr[i], lmax[i-1]);
+        }
+
+        for(int i = arr.length-2; i>=0; i--){
+            rmax[i] = Math.max(arr[i], rmax[i+1]);
+        }
+
+        for(int i = 0; i< arr.length; i++){
+            res += Math.min(lmax[i], rmax[i]) - arr[i];
+        }
+
+        return res;
+    }
 }
